01d589599e4dd3b564ba3f6612b48c011d01f68f
Alejandro Sulbaran <sulbaran.kun@gmail.com>
2018-05-10 13:28:50 -0400

    Adding stubs and proxyquires

--- tests/agent-tests.js
+++ tests/agent-tests.js
@@ -1,21 +1,53 @@
 'use strict'
 
 const test = require('ava')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
 /* here we are adding a ligging function to the config in memory */
 let config = {
   logging: function () {}
 }
 
-let db = null // here we are declaring the db global var so we can use it in the test
+/** Creating the metric stub that will overwrite the real one in order to use it for tests porpuses
+ * here we are using the sinon module to know if the instance has been called
+ */
+let MetricStub = {
+  belongsTo: sinon.spy()
+}
 
-/* Connecting to the databse before each test */
+let AgentStub = null
+let db = null // here we are declaring the db global var so we can use it in the test
+let sandbox = null
+
+/** Connecting to the databse before each test
+ * we create a new sinon sandbox and assing it to the AgentStub
+ * before that, we are using proxyquire to overwrite the functions of the real models
+*/
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.createSandbox()
+
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
+
   db = await setupDatabase(config)
 })
 
 /* here we are making the real test of the Agent model with the Ava module */
 test('Agent', t => {
   t.truthy(db.Agent, 'Agent service should exist')
+})
+
+/* Serial tests using sinon */
+test.serial('Setup', t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
 })
