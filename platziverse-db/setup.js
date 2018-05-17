13aba3c9e22eb5844679fa76e082f2c29790450f
Alejandro Sulbaran <sulbaran.kun@gmail.com>
2018-05-17 14:54:01 -0400

    Revert "[db] arguments for automatic deploy added to our setup script"
    This reverts commit c9eb218bc5f7051cdddbabf9091e9da370ffad05.

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -4,24 +4,21 @@ const debug = require('debug')('platziverse:db:setup')
 const inquirer = require('inquirer')
 const chalk = require('chalk')
 const db = require('./')
-const argv = require('yargs').argv
 
 const prompt = inquirer.createPromptModule()
 
 async function setup () {
-  if (!argv.y) {
-    const answer = await prompt([
-      {
-        type: 'confirm',
-        name: 'setup',
-        message: 'This will destroy your databsae, are you sure?'
-      }
-    ])
-    if (!answer.setup) {
-      return console.log('Nothing happened :)')
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your databsae, are you sure?'
     }
-  }
+  ])
 
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
